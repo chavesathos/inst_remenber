aca18b9292d5bc3cb8a46c05e1f3e69359d794bd
Athos Chaves <a.thos_chaves@hotmail.com>
2021-03-21 23:58:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Notifyer } from './Notifyer.js';
+import { Timer } from './Timer.js';
+import { Emitter} from './Emitter.js';
+
+const messages = [
+    "Postar foto do meu setup",
+    "Postar uma frase motivacional",
+    "Ensinar algo que aprendi hoje ou ontem",
+    "Crie conteúdo para agregar valor"
+];
+
+const notify = () => {
+    
+    const randomIndex = Math.floor(Math.random() * messages.length)
+
+    const notification = Notifyer.notify({
+    title: "Hora do Post",
+    body:  messages[randomIndex] 
+ })
+
+ notification()
+}
+
+const App = {
+  async start() {
+    try {
+        await Notifyer.init()
+
+        Emitter.on('countdown-start', notify)
+        Emitter.on('countdown-end', Timer.init)
+
+      Timer.init()
+    }catch (err) {
+        console.log(err.message)
+    }
+  }
+}
+
+export { App }
